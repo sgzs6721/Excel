4f6cc2bedab96bb5e45e8a6b7a1ce0163af498ba
martin <sgzs6721@126.com>
2014-08-22 17:02:04 +0800

    excel operation

--- application/controllers/excel.php
+++ application/controllers/excel.php
@@ -17,8 +17,34 @@ class Excel extends MY_Controller {
 
 		$PHPExcel = $PHPReader->load($filePath);
 
+
+
+
 		$objWriter = PHPExcel_IOFactory::createWriter($PHPExcel, 'Excel2007');
 		$objWriter->save('uploads/test.xlsx');
+
+		echo 'test';
+    }
+
+    function test()
+    {
+        $objPHPExcel = PHPExcel_IOFactory::load("uploads/WebUI_V2_Testcases.xls","Excel5");
+        echo 'read excel<br>';
+
+        $sheet = $objPHPExcel->getSheet(0);
+
+        $highestRow = $sheet->getHighestRow();
+        
+        echo $highestRow.'<br>';
+
+        echo $sheet->getCell('A24')->getValue().'<br>';
+
+        $sheet->setCellValue('A24', '5');
+
+        echo 'set value 5 for A24';
+        $objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel5');
+
+        $objWriter->save('uploads/test.xls');
     }
 }
 
